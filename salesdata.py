3064f032f034c40db55934ebd67af880a078cc78
mazek1 <madskildelund@gmail.com>
2025-03-10 11:24:15 +0100

    Update salesdata.py

--- salesdata.py
+++ salesdata.py
@@ -105,12 +105,12 @@ if df is not None:
         if total_sales_customer.empty:
             st.warning(f"Ingen salg registreret for {selected_customer} i denne periode.")
         else:
-        fig, ax = plt.subplots()
-        total_sales_customer.plot(kind="line", ax=ax)
-        ax.set_title(f"Sales Over Time for {selected_customer}")
-        ax.set_ylabel("Sales (DKK)")
-        ax.set_xlabel("Date")
-        st.pyplot(fig)
+            fig, ax = plt.subplots()
+            total_sales_customer.plot(kind="line", ax=ax)
+            ax.set_title(f"Sales Over Time for {selected_customer}")
+            ax.set_ylabel("Sales (DKK)")
+            ax.set_xlabel("Date")
+            st.pyplot(fig)
 if df is not None and not adgang_alle:
     df["Salesperson"] = df["Salesperson"].astype(str).str.lower().str.strip()
     df["Salesperson"] = df["Salesperson"].replace({'\\r': '', '\\n': ''}, regex=True)
